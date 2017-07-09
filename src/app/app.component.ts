f3ebd3a6524c3e8f9ce8267ccb6f2d86ff9fb6a4
Missak Boyajian <missakboya@live.com>
2017-07-10 01:32:04 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { RankingPage } from '../pages/ranking/ranking';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,14 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Ranking', component: RankingPage },
+      { title: 'News', component: HomePage },
+      { title: 'Events', component: HomePage },
+      { title: 'Committe', component: HomePage },
+      { title: 'Archive', component: HomePage },
+      { title: 'Players', component: HomePage },
+      { title: 'Calendar', component: HomePage },
+      
     ];
 
   }
